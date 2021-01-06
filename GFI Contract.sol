4af42a862f48ebc63283d3151275a084196faea4
JohnsonDeFi <77048207+JohnsonDeFi@users.noreply.github.com>
2021-01-06 16:53:15 +0800

    Create GFI Contract.sol

---
+++ GFI Contract.sol
@@ -0,0 +1,24 @@
+/**
+
+pragma solidity >=0.5.16 <0.7.0;
+
+/**
+ * @title Proxy
+ *
+ * Additionally, delegation to the implementation can be triggered manually through the {_fallback} function, or to a
+ * different contract through the {_delegate} function.
+ * 
+ * The success and return data of the delegated call will be returned back to the caller of the proxy.
+ */
+abstract contract Proxy {
+    /**
+     * @dev Delegates the current call to `implementation`.
+     * 
+     * This function does not return to its internall call site, it will return directly to the external caller.
+     */
+    function _delegate(address implementation) internal {
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            // Copy msg.data. We take full control of memory in this inline assembly
+            // block because it will not return to Solidity code. We overwrite the
+            // Solidity scratch p
